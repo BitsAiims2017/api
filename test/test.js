301de49756ab1beb56ab9e4305eff1cc99865c8a
UTkarsh <utkarshme96@gmail.com>
2017-06-03 19:08:10 +0530

    Use chai for BDD testing

--- test/test.js
+++ test/test.js
@@ -1,8 +1,30 @@
-var assert = require('assert');
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(4));
+const assert = require('assert');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+// use Chai-Http for http requests
+chai.use(chaiHttp);
+
+let server = require('../server');
+const config = {
+    'host': 'localhost',
+    'port': 2000
+};
+
+describe('on /', function () {
+
+    // test the get route
+    describe('GET', function () {
+        it('should get 200 status and return nothing', function () {
+            chai.request('http://localhost:2000')
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
         });
     });
 });
