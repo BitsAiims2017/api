7549ab2530132844c88fb49336f14bc501d609f3
UTkarsh <utkarshme96@gmail.com>
2017-06-07 00:04:19 +0530

    Add basic documentation setup
    - Squash this since, it's basically useless, before merging

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
 
+/**
+ * @apiName AIIMS IMS
+ * @apiGroup General
+ * @api {get} / Request something
+ *
+ * @apiParam {Number} id ID of something
+ * @apiParam {Number} num Total number of somethings
+ *
+ *
+ * @apiSuccess {String} something Something
+ * @apiSuccess {Number} number Total number of something returned
+ * @apiSuccessExample {json} Success:
+ *    {
+ *      "something": "This is something",
+ *      "number": 10
+ *    }
+ *
+ * @apiError SomethingNotFound No something of that ID was found
+ * @apiErrorExample {json} Error:
+ *    {
+ *      "error": "error msg"
+ *    }
+ */
 router.route('/')
     .get((req, res) => {
         // handle get request
