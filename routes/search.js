9fe6cfffb0c0164e99d5fea9afba7bec1b058d22
UTkarsh <utkarshme96@gmail.com>
2017-06-20 02:40:10 +0530

    Add documentation for search

--- routes/search.js
+++ routes/search.js
@@ -15,6 +15,30 @@ const validate = require('../lib/validate.js');
 router.use(auth.decode_token);
 
 router.route('/users').
+    /**
+     * @apiGroup Search
+     * @apiVersion 0.0.1
+     *
+     * @api {get} /search/users?params 1.1 Search for users
+     * @apiParam query The query to search for
+     * @apiParam limit The number of items in result
+     * @apiParam sort The field to sort according to
+     * @apiParam order The order to sort with 'asc' or 'desc'
+     *
+     * @apiSuccessExample {json} Success:
+     *  {
+     *      "status": "Status code",
+     *      "data": [{Objects}]
+     *  }
+     *
+     * @apiError 401 The request is not authorized
+     *
+     * @apiErrorExample {json} Error:
+     *  {
+     *      "status": "Error status code",
+     *      "message": "Error Message"
+     *  }
+     */
     get(auth.authenticate({role: 'viewer'}), (req, res) => {
         lib.search_users(req.query, (data) => {
             res.status(data.status).send(data);
@@ -22,6 +46,30 @@ router.route('/users').
     });
 
 router.route('/items').
+    /**
+     * @apiGroup Search
+     * @apiVersion 0.0.1
+     *
+     * @api {get} /search/items?params 1.2 Search for items
+     * @apiParam query The query to search for
+     * @apiParam limit The number of items in result
+     * @apiParam sort The field to sort according to
+     * @apiParam order The order to sort with 'asc' or 'desc'
+     *
+     * @apiSuccessExample {json} Success:
+     *  {
+     *      "status": "Status code",
+     *      "data": [{Objects}]
+     *  }
+     *
+     * @apiError 401 The request is not authorized
+     *
+     * @apiErrorExample {json} Error:
+     *  {
+     *      "status": "Error status code",
+     *      "message": "Error Message"
+     *  }
+     */
     get(auth.authenticate({role: 'viewer'}), (req, res) => {
         lib.search_items(req.query, (data) => {
             res.status(data.status).send(data);
