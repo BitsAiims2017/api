b27ca3fbda2cc1f464ff86e3adc4f097a3af700e
UTkarsh <utkarshme96@gmail.com>
2017-06-21 13:52:43 +0530

    Serve favicon and set correct headers

--- server.js
+++ server.js
@@ -7,17 +7,30 @@ const logger = require('morgan');
 const cookie_parser = require('cookie-parser');
 const body_parser = require('body-parser');
 const config = require('./config/default.js');
+const favicon = require('serve-favicon');
 const port = process.env.PORT || config.port;
 const host = process.env.HOST || config.host;
 
 
+// add a logger if env is not test
 if (process.env.NODE_ENV !== 'test') {
-    app.use(logger('dev')); // add a logger
+    app.use(logger('dev'));
 }
 
 // set json properties
 app.set('json spaces', 2);
 app.locals.pretty = true;
+
+// set req and res properties
+app.use((req, res, next) => {
+    req.accepts(['html', 'json']);
+    res.set('Content-type', 'application/json');
+    res.type('json');
+    next();
+} );
+
+// add a favicon
+app.use(favicon(__dirname + '/data/favicon.png'));
 
 // add cookie and body parsers
 app.use(cookie_parser(config.cookie_secret));
@@ -31,6 +44,11 @@ app.use('/items', require('./routes/item.js'));
 app.use('/reports', require('./routes/report.js'));
 app.use('/auth', require('./routes/auth.js'));
 app.use('/search', require('./routes/search.js'));
+
+// send 'bad request' if no route found
+app.use('*', (req, res, next) => {
+    res.status(400).send({status: 400, message: 'Bad request'});
+});
 
 // start the app
 app.listen(port, host, () => {
